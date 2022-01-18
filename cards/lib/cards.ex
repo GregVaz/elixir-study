5d4e6153e8a319db756d085c5d764f944f3adfbe
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-01-18 15:27:28 -0700

    A bad nested list implementation with list comprehensions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -13,7 +13,14 @@ defmodule Cards do
 
   """
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diaments"]
+
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
   end
 
   @doc """
