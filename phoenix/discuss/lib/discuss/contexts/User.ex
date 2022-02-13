f7001d2447ab86b430501af0fc8f50a1d07bd24a
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-02-12 22:19:19 -0700

    feat: add user schema and migration

---
+++ phoenix/discuss/lib/discuss/contexts/User.ex
@@ -0,0 +1,25 @@
+defmodule Discuss.Contexts.User do
+  use Discuss.Schema
+
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  @required_fields [
+    :email,
+    :provider,
+    :token
+  ]
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+  end
+end
