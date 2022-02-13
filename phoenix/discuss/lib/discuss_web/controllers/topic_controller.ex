ec59798417c3c28b7041bb4ee8925e49baa52ad8
Gregorio Vazquez <gregoriovazya@gmail.com>
2022-02-13 12:21:07 -0700

    feat: create a topic with its user owner

--- phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,5 +1,6 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  import Ecto, warn: false
 
   alias Discuss.Repo
 
@@ -18,7 +19,10 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset =
+      conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _post} ->
